82a47152788e803119fca0859b8c61b04cd58c7c
Manuel de Almeida <ManuelAlmeida70@outlook.pt>
2026-03-21 11:34:48 +0100

    get method of customerControllers done

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,8 +1,21 @@
 'use-strict'
 
 import customerRepository from '../repositories/customerRepository'
+import validationContract from '../validators/fluent-validator'
 
 export const get = async(req, res, next) => {
+
+    let contract = new validationContract();
+
+    contract.isEmail(req.body.email, "Email invalido");
+
+    if (!contract.isValid())
+    {
+        res.status.send(contract.errors()).end();
+        return;
+    }
+
+
     try
     {
         const data = await customerRepository.get(req.body.email, req.body.password);
